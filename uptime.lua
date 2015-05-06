6bab59827f8d768c6e58093f6f8cf2b1b9b3b194
jebjerg <casper@jebjerg.eu>
2015-05-06 20:59:29 +0200

    use rex instead of useless string for regexp. fix hook breaking change.

--- uptime.lua
+++ uptime.lua
@@ -1,8 +1,10 @@
+rex = require "rex_pcre"
+
 function check_uptime(channel, text)
-    if string.match(text, "^(uptime|status)") ~= nil then
+    if rex.match(text, "^(uptime|status)") ~= nil then
         uptime = io.popen("uptime"):read("*a")
         bot.privmsg(channel, "uptime: "..uptime)
     end
 end
 
-bot.hook("PRIVMSG", check_uptime)
+bot.hook("PRIVMSG", "check_uptime")
